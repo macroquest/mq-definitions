bf18b80fb35599b9d86b5d339d479bb4f0f7aeb7
Martin Lindhe <martin@ubique.se>
2022-09-13 15:14:36 +0200

    FrameLimiter: fix definition of primitive types to be fun():type

--- datatype/_framelimiter.lua
+++ datatype/_framelimiter.lua
@@ -1,11 +1,11 @@
 ---@class framelimiter
----@field public BackgroundFPS number Value of the target background fps setting.
----@field public ClearScreen boolean Value of the clear screen when not rendering setting.
----@field public CPU number Current CPU usage as %
----@field public Enabled boolean TRUE if the frame limiter feature is currently active.
----@field public ForegroundFPS number Value of the target foreground fps setting.
----@field public MinSimulationFPS number Value of the minimum simualtion rate setting.
----@field public RenderFPS number Current graphics scene frame rate (visible fps).
----@field public SaveByChar boolean TRUE if settings for the frame limiter are being saved by character.
----@field public SimulationFPS number Current simulation frame rate (game updates per second).
----@field public Status string Either "Foreground" or "Background".
+---@field public BackgroundFPS fun():number Value of the target background fps setting.
+---@field public ClearScreen fun():boolean Value of the clear screen when not rendering setting.
+---@field public CPU fun():number Current CPU usage as %
+---@field public Enabled fun():boolean TRUE if the frame limiter feature is currently active.
+---@field public ForegroundFPS fun():number Value of the target foreground fps setting.
+---@field public MinSimulationFPS fun():number Value of the minimum simualtion rate setting.
+---@field public RenderFPS fun():number Current graphics scene frame rate (visible fps).
+---@field public SaveByChar fun():boolean TRUE if settings for the frame limiter are being saved by character.
+---@field public SimulationFPS fun():number Current simulation frame rate (game updates per second).
+---@field public Status fun():string Either "Foreground" or "Background".

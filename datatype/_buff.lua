bc0467b01d9a1123e0e64bf569d3fe0dfb214b86
Aleksander Spro <abspro@gmail.com>
2022-09-16 15:50:21 +0200

    Buff inherits spell

--- datatype/_buff.lua
+++ datatype/_buff.lua
@@ -1,4 +1,4 @@
---- @class buff
+--- @class buff : spell
 --- @field public Counters MQFloat #The number of counters added by the buff
 --- @field public Dar MQFloat #The remaining damage absorption of the buff (if any). This is not entirely accurate, it will only show you to the Dar of your spell when it was initially cast, or what it was when you last zoned (whichever is more recent).
 --- @field public Duration ticks The time remaining before the buff fades (not total duration)

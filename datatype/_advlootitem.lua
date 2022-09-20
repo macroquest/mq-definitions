9ec636af02a779e0665434c593ff702dae402013
John <414568+johnfking@users.noreply.github.com>
2022-09-20 10:04:01 -0400

    fix: uses alias in place of repetative code

--- datatype/_advlootitem.lua
+++ datatype/_advlootitem.lua
@@ -5,12 +5,12 @@
 ---@field public AutoRoll fun(): boolean The Auto Roll state (dice icon) of the item.
 ---@field public Corpse	MQSpawn The spawn representing the corpse that is being looted, if available.
 ---@field public Greed fun(): boolean The Greed (GD) state of the item.
----@field public IconID fun(): integer	The ID of the icon for the item.
----@field public ID fun(): integer The ID of the item.
----@field public Index fun(): integer The positional index of the item.
+---@field public IconID MQInt	The ID of the icon for the item.
+---@field public ID MQInt The ID of the item.
+---@field public Index MQInt The positional index of the item.
 ---@field public Name fun(): string The name of the item.
 ---@field public Need fun(): boolean The Need (ND) state of the item.
 ---@field public Never fun(): boolean The Never (NV) state of the item.
 ---@field public No fun(): boolean No	The No state of the item.
 ---@field public NoDrop fun(): boolean	Indicates if the item is NO DROP.
----@field public StackSize fun(): integer	The size of the stack of items being looted.
+---@field public StackSize MQInt	The size of the stack of items being looted.
--- datatype/_itemfilterdata.lua
+++ datatype/_itemfilterdata.lua
@@ -2,9 +2,9 @@
 ---@class itemfilterdata
 ---@field AutoRoll fun(): boolean The Auto Roll state (dice icon)
 ---@field Greed fun(): boolean The Greed (GD) state
----@field IconID fun(): integer The ID of the icon
----@field ID fun(): integer The ID of the item
+---@field IconID MQInt The ID of the icon
+---@field ID MQInt The ID of the item
 ---@field Name fun(): string The Name of the item
 ---@field Need fun(): boolean The Need (ND) state
 ---@field Never fun(): boolean The Never (NV) state
----@field Types fun(): integer Bit field representing all the loot filter flags for this item
+---@field Types MQInt Bit field representing all the loot filter flags for this item

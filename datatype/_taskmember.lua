0bb867fd7923963c99a944fb3e06d7665be3ee52
Aleksander Spro <abspro@gmail.com>
2022-09-20 11:36:56 +0200

    Minor cleanup

--- datatype/_taskmember.lua
+++ datatype/_taskmember.lua
@@ -1,4 +1,4 @@
 --- @class taskmember
---- @field public Name string Returns name of the member
---- @field public Leader boolean Returns true if the member is leader
---- @field public Index number Returns task index for member (i.e. 1-6)
+--- @field public Name MQString # Returns name of the member
+--- @field public Leader MQBoolean # Returns true if the member is leader
+--- @field public Index MQInt # Returns task index for member (i.e. 1-6)

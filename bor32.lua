57b8ff37319f7788b116b7c6e65394f28ce80c70
Aleksander Spro <abspro@gmail.com>
2022-11-17 18:35:06 +0100

    Add bor32 def

---
+++ bor32.lua
@@ -0,0 +1,10 @@
+---@meta
+
+---@class bor32
+bor32 = {}
+
+---Performs bit AND with all arguemnts.
+---@generic T
+---@vararg T # List of flags to AND
+---@return T # AND result of arguments
+function bor32.bor(...) end

bc43474573f2dee7f227a51a6301724f4d0455ba
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2026-02-15 10:41:09 -0500

    Fix .Remove() for auras

--- mq/datatype/_auratype.lua
+++ mq/datatype/_auratype.lua
@@ -1,9 +1,7 @@
 ---@meta
 
 ---@class auratype Describes an auratype
----@field public ID MQInt ID of the Aura 
----@field public Name MQString Name of the Aura
----@field public SpawnID MQInt ID of the spawn that emits auralocal auratype = nil
-
----Removes the aura by index or name
-function auratype.Remove() end
+---@field public ID MQInt # ID of the Aura
+---@field public Name MQString # Name of the Aura
+---@field public SpawnID MQInt # ID of the spawn that emits aura
+---@field public Remove fun(): nil # Removes the aura
